674c6492a5a26299a7e33d41179d602f927616e1
Murat Simsek <muratsimseekk@gmail.com>
2024-02-11 23:34:32 +0300

    adds carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import TopSlider from "./HomePage/TopSlider";
 import Content from "./HomePage/Content";
+import Footer from "./HomePage/Footer";
 
 function HomePage() {
   return (
     <div>
       <TopSlider />
       <Content />
-      <TopSlider />
+      <Footer />
     </div>
   );
 }
--- src/pages/HomePage/TopSlider.jsx
+++ src/pages/HomePage/TopSlider.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import AwesomeSlider from "react-awesome-slider";
 import slider1 from "./../../assets/img1.jpg";
 import slider2 from "./../../assets/img2.jpg";
 
+import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
-import "react-awesome-slider/dist/custom-animations/cube-animation.css";
+import "react-awesome-slider/dist/custom-animations/open-animation.css";
 
 function TopSlider() {
   return (
-    <AwesomeSlider className="" animation="cubeAnimation">
-      <div data-src={slider1} />
-      <div data-src={slider2} />
-    </AwesomeSlider>
+    <>
+      <AwesomeSlider animation="openAnimation">
+        <div data-src={slider1} />
+        <div data-src={slider2} />
+      </AwesomeSlider>
+    </>
   );
 }
 
