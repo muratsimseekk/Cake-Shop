ba6e2f2317d9cce724a8e96a091a689623817331
Murat Simsek <muratsimseekk@gmail.com>
2024-02-12 20:49:42 +0300

    adds cards section styling

--- src/pages/HomePage/Content.jsx
+++ src/pages/HomePage/Content.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import slider1 from "./../../assets/img1.jpg";
+
+import Cards from "./ContentPages/Cards";
 
 function Content() {
   const viewButton = (e) => {
@@ -8,60 +9,7 @@ function Content() {
   };
   return (
     <div className="bg-slate-500 py-10 flex w-full justify-center">
-      <div className="w-4/5 bg-red-200">
-        <div>
-          <h3 className="tracking tracking-wider text-2xl py-10 ">
-            Our Valentines range ...
-          </h3>
-        </div>
-        <div className="flex flex-wrap justify-between">
-          <div className="flex flex-col gap-4 border w-[24%]">
-            <div className=" w-full">
-              <img src={slider1} alt="" />
-            </div>
-            <div className="flex flex-col gap-2 text-xl">
-              <h3>Valentines Treat Box</h3>
-              <p>$16.00 </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 border w-[24%]">
-            <div className=" w-full">
-              <img src={slider1} alt="" />
-            </div>
-            <div className="flex flex-col gap-2 text-xl">
-              <h3>Valentines Treat Box</h3>
-              <p>$16.00 </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 border w-[24%]">
-            <div className=" w-full">
-              <img src={slider1} alt="" />
-            </div>
-            <div className="flex flex-col gap-2 text-xl">
-              <h3>Valentines Treat Box</h3>
-              <p>$16.00 </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 border w-[24%]">
-            <div className=" w-full">
-              <img src={slider1} alt="" />
-            </div>
-            <div className="flex flex-col gap-2 text-xl">
-              <h3>Valentines Treat Box</h3>
-              <p>$16.00 </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-center py-4 bg-slate-400 mt-10">
-          <button
-            type="button"
-            onClick={(e) => viewButton(e)}
-            className="bg-red-600 py-1 px-5 shadow-md cursor-pointer focus:hidden"
-          >
-            View All
-          </button>
-        </div>
-      </div>
+      <Cards />
     </div>
   );
 }
