a0d6448a3c0ce4d933cff2d3e79c24c261200a27
Murat Simsek <muratsimseekk@gmail.com>
2024-03-20 21:42:48 +0300

    completes Contact page's responsive desing for mobile devices

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
     <div className="w-full flex justify-center">
       <div className="w-4/5 bg-blue-300 my-16 flex flex-col gap-16">
         <div className="flex flex-col gap-7 text-center">
-          <h1 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl">
+          <h1 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl text-3xl">
             Get In Touch
           </h1>
           <div className="flex flex-col gap-3 2xl:text-xl xl:text-lg lg:text-lg md:text-base">
@@ -22,11 +22,11 @@ function Contact() {
           </div>
         </div>
         <div className="flex justify-center">
-          <div className="2xl:w-[60%] xl:w-[60%] lg:w-[65%] md:w-[80%]  bg-red-300 flex flex-col gap-4">
-            <h3 className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-lg">
+          <div className="2xl:w-[60%] xl:w-[60%] lg:w-[65%] md:w-[80%] w-full  bg-red-300 flex flex-col gap-4">
+            <h3 className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-lg text-lg">
               Make an enquiery
             </h3>
-            <div className="flex justify-between">
+            <div className="hidden md:flex md:justify-between lg:flex lg:justify-between xl:flex xl:justify-between 2xl:flex 2xl:justify-between">
               <input
                 className="border border-black w-[47%] py-2 pl-5"
                 type="text"
@@ -40,6 +40,19 @@ function Contact() {
                 placeholder="E-mail *"
               />
             </div>
+            <input
+              className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
+              type="text"
+              placeholder="Name *"
+            />
+            <input
+              className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
+              type="email"
+              name=""
+              id=""
+              placeholder="E-mail *"
+            />
+
             <input
               className="border border-black w-full py-2 pl-5"
               type="tel"
@@ -60,20 +73,36 @@ function Contact() {
             </button>
           </div>
         </div>
-        <div className="flex flex-col gap-2 text-center 2xl:text-lg xl:text-lg lg:text-lg md:text-base">
+        <div className="flex flex-col gap-2 text-center 2xl:text-lg xl:text-lg lg:text-lg md:text-base text-sm">
           <p>30 Market Street ,Adlinghton , Chorley , PR7 4HE</p>
           <p>01257 781782</p>
           <p>muratsimseekk@gmail.com</p>
         </div>
         <div className="w-full flex flex-col gap-10">
-          <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl bg-red-400 text-center">
+          <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl text-xl bg-red-400 text-center">
             Follow on instagram
           </h3>
           <div className="flex w-full justify-between">
-            <img className="w-[24%] rounded-md" src={photo4} alt="" />
-            <img className="w-[24%] rounded-md" src={photo5} alt="" />
-            <img className="w-[24%] rounded-md" src={photo6} alt="" />
-            <img className="w-[24%] rounded-md" src={photo7} alt="" />
+            <img
+              className="md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%] w-[33%] rounded-md"
+              src={photo4}
+              alt=""
+            />
+            <img
+              className="md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%] w-[33%] rounded-md"
+              src={photo5}
+              alt=""
+            />
+            <img
+              className="md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%] w-[33%] rounded-md"
+              src={photo6}
+              alt=""
+            />
+            <img
+              className="w-[24%] rounded-md md:block lg:block xl:block 2xl:block hidden"
+              src={photo7}
+              alt=""
+            />
           </div>
         </div>
       </div>
