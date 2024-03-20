b0ad11e1cfa14ab69ab1b3109af56829bd6149ab
Murat Simsek <muratsimseekk@gmail.com>
2024-03-20 18:44:03 +0300

    completes wedding page's responsive design for mobile devices

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -14,33 +14,33 @@ function Wedding() {
     <div className="w-full flex justify-center">
       <div className="w-4/5 bg-yellow-300 my-14 flex flex-col gap-10">
         <div className="flex flex-col gap-6">
-          <h2 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl text-center">
+          <h2 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl text-3xl text-center">
             Weddings
           </h2>
           <p className="2xl:text-2xl xl:text-xl lg:text-xl md:text-base text-center">
             For your perfect day,you deserve the perfect cake
           </p>
         </div>
-        <div className="flex justify-between h-[900px]">
-          <div className="w-[65%] h-full">
+        <div className="flex flex-col  md:flex md:flex-row md:justify-between md:h-[900px] lg:flex lg:flex-row lg:justify-between lg:h-[900px] xl:flex xl:flex-row xl:justify-between xl:h-[900px] 2xl:flex 2xl:flex-row 2xl:justify-between 2xl:h-[900px]">
+          <div className="w-full md:w-[65%] lg:w-[65%] xl:w-[65%] 2xl:w-[65%] ">
             <img
               className="w-full h-full object-fill rounded-md"
               src={photo1}
             />
           </div>
-          <div className="w-[34%] flex flex-col justify-between">
+          <div className=" flex  md:w-[34%] lg:w-[34%] xl:w-[34%] 2xl:w-[34%] md:flex  md:flex-col md:justify-between lg:flex lg:flex-col lg:justify-between xl:flex  xl:flex-col xl:justify-between 2xl:flex  2xl:flex-col 2xl:justify-between">
             <img
-              className="w-full h-[49%] object-fill rounded-md"
+              className="w-1/2 md:w-full lg:w-full xl:w-full 2xl:w-full md:h-[49%] lg:h-[49%] xl:h-[49%] 2xl:h-[49%] object-fill rounded-md"
               src={photo2}
             />
             <img
-              className="w-full h-[49%] object-fill rounded-md"
+              className="w-1/2 md:w-full lg:w-full xl:w-full 2xl:w-full md:h-[49%] lg:h-[49%] xl:h-[49%] 2xl:h-[49%] object-fill rounded-md"
               src={photo3}
             />
           </div>
         </div>
         <div className="flex flex-col gap-5 text-center items-center ">
-          <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl">
+          <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl text-2xl">
             It's time to create something you've only ever dreamt of{" "}
           </h3>
           <p className="2xl:text-base xl:text-base lg:text-base md:text-[15px]">
@@ -51,31 +51,31 @@ function Wedding() {
             My Wedding Brochure will provide you with all the details you need
             to know .
           </p>
-          <button className="px-5 py-1 max-w-max border border-rose-400 rounded-md">
+          <button className="px-5 py-1.5 max-w-max border border-rose-400 rounded-md">
             Download Brochure{" "}
           </button>
         </div>
         <div className="flex justify-between">
-          <div className="w-[24%]">
+          <div className="w-[32.5%] md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%]">
             <img className="w-full rounded-md" src={photo4} />
           </div>
-          <div className="w-[24%]">
+          <div className="w-[32.5%] md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%]">
             <img className="w-full rounded-md" src={photo5} />
           </div>
-          <div className="w-[24%]">
+          <div className="w-[32.5%] md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%]">
             <img className="w-full rounded-md" src={photo6} />
           </div>
-          <div className="w-[24%]">
+          <div className="hidden md:block lg:block xl:block 2xl:block md:w-[24%] lg:w-[24%] xl:w-[24%] 2xl:w-[24%]">
             <img className="w-full rounded-md" src={photo7} />
           </div>
         </div>
-        <div className="flex justify-between ">
-          <div className="w-1/2 ">
+        <div className="flex flex-col gap-8  md:flex md:flex-row md:justify-between lg:flex lg:flex-row lg:justify-between xl:flex xl:flex-row xl:justify-between 2xl:flex 2xl:flex-row 2xl:justify-between">
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 ">
             <img className="w-full rounded-md" src={photo8} />
           </div>
-          <div className="w-1/2 flex justify-center items-center ">
+          <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 text-center md:text-start lg:text-start xl:text-start 2xl:text-start flex justify-center items-center ">
             <div className="bg-red-300 w-4/5 flex flex-col gap-8">
-              <h2 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl">
+              <h2 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl text-2xl">
                 Let's talk it over ...
               </h2>
               <div className="flex flex-col gap-4">
@@ -95,11 +95,11 @@ function Wedding() {
           </div>
         </div>
         <div className="flex justify-center">
-          <div className="2xl:w-[60%] xl:w-[60%] lg:w-[65%] md:w-[80%]  bg-red-300 flex flex-col gap-4">
+          <div className="2xl:w-[60%] xl:w-[60%] lg:w-[65%] md:w-[80%] w-full  bg-red-300 flex flex-col gap-4">
             <h3 className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-lg">
               Make an enquiery
             </h3>
-            <div className="flex justify-between">
+            <div className="hidden md:flex md:justify-between lg:flex lg:justify-between xl:flex xl:justify-between 2xl:flex 2xl:justify-between">
               <input
                 className="border border-black w-[47%] py-2 pl-5"
                 type="text"
@@ -113,6 +113,19 @@ function Wedding() {
                 placeholder="E-mail *"
               />
             </div>
+            <input
+              className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
+              type="text"
+              placeholder="Name *"
+            />
+            <input
+              className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
+              type="email"
+              name=""
+              id=""
+              placeholder="E-mail *"
+            />
+
             <input
               className="border border-black w-full py-2 pl-5"
               type="tel"
@@ -133,12 +146,12 @@ function Wedding() {
             </button>
           </div>
         </div>
-        <div className="bg-blue-400 flex flex-col gap-2 text-center mt-9 text-lg">
+        <div className="bg-blue-400 hidden md:flex md:flex-col lg:flex lg:flex-col xl:flex xl:flex-col 2xl:flex 2xl:flex-col gap-2 text-center mt-9 text-lg">
           <p>30 Market Street , Adlinhton , Chorley,PR7 4HE</p>
           <p>123456789</p>
           <p>muratsimseekk@gmail.com</p>
         </div>
-        <div className="w-full flex flex-col gap-10">
+        <div className="w-full hidden md:flex md:flex-col lg:flex lg:flex-col xl:flex xl:flex-col 2xl:flex 2xl:flex-col gap-10">
           <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl bg-red-400 text-center">
             Follow on instagram
           </h3>
