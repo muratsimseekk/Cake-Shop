2889aea5975d6b1d25776a2910c9cf90d696223f
Murat Simsek <muratsimseekk@gmail.com>
2024-06-21 10:53:21 +0300

    Redesing Contact page's texts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,27 +13,27 @@ function Contact() {
       <div className="w-4/5 my-16 flex flex-col gap-16">
         <div className="flex flex-col gap-7 text-center">
           <h1 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl text-3xl">
-            Get In Touch
+            Bizimle İletişime Geçin
           </h1>
           <div className="flex flex-col gap-3 2xl:text-xl xl:text-lg lg:text-lg md:text-base">
-            <p>Please ensure you include the following in your enqueries .</p>
-            <p>This helps to ensure you get a quote as quick as possible:</p>
-            <p>--The date you would like to collect </p>
-            <p>--Cake size or quantity of treats </p>
-            <p>--Flavours </p>
-            <p>--Design ideas and any inspiration</p>
+            <p>Lütfen aşağıdaki bilgileri sorularınıza dahil edin..</p>
+            <p>Bu, en kısa sürede bir fiyat teklifi almanızı sağlar:</p>
+            <p>--Teslim almak istediğiniz tarih </p>
+            <p>--Pasta boyutu veya ikramların miktarı </p>
+            <p>--İçerik </p>
+            <p>--Tasarım fikirleri </p>
           </div>
         </div>
         <div className="flex justify-center">
           <div className="2xl:w-[60%] xl:w-[60%] lg:w-[65%] md:w-[80%] w-full flex flex-col gap-4">
-            <h3 className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-lg text-lg">
-              Make an enquiery
+            <h3 className="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-lg text-lg text-center">
+              Fiyat almak için formu doldurun
             </h3>
             <div className="hidden md:flex md:justify-between lg:flex lg:justify-between xl:flex xl:justify-between 2xl:flex 2xl:justify-between">
               <input
                 className="border border-black w-[47%] py-2 pl-5"
                 type="text"
-                placeholder="Name *"
+                placeholder="İsim Soyisim *"
               />
               <input
                 className="border border-black w-[47%] py-2 pl-5"
@@ -46,7 +46,7 @@ function Contact() {
             <input
               className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
               type="text"
-              placeholder="Name *"
+              placeholder="İsim Soyisim *"
             />
             <input
               className="block md:hidden lg:hidden xl:hidden 2xl:hidden border border-black w-full py-2 pl-5"
@@ -61,7 +61,7 @@ function Contact() {
               type="tel"
               name=""
               id=""
-              placeholder="Phone Number *"
+              placeholder="Telefon Numarası *"
             />
             <textarea
               className="border border-black w-full pt-2 pl-5"
@@ -69,17 +69,18 @@ function Contact() {
               id=""
               cols="30"
               rows="5"
-              placeholder="Comment"
+              placeholder="Detaylar"
             ></textarea>
             <button className="max-w-max border border-black py-1 px-10 rounded-md">
-              Send
+              Gönder
             </button>
           </div>
         </div>
         <div className="flex flex-col gap-2 text-center 2xl:text-lg xl:text-lg lg:text-lg md:text-base text-sm">
-          <p>30 Market Street ,Adlinghton , Chorley , PR7 4HE</p>
-          <p>01257 781782</p>
-          <p>muratsimseekk@gmail.com</p>
+          <p>Batıkent Mahallesi Abdulkadir Aksu Bulvarı No : 56/D </p>
+          <p>Şehitkamil/Gaziantep</p>
+          <p>+90 538 320 77 97</p>
+          <p>tugce_simsek2017@outlook.com</p>
         </div>
         <div className="w-full flex flex-col gap-10">
           <h3 className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-xl text-xl text-center">
